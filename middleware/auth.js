ff9e3b76be82b7a128f7f9d470a086877dfbddc0
Yevhwnii <mirandoos3223@gmail.com>
2020-08-03 15:59:08 +0300

    # renamed and updated auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,9 +7,8 @@ const SECRET_KEY = process.env.JWT_SECRET_KEY;
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    const error = new Error('Not authenticated');
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
   // Since token is Bearer + token we split on whitespace and pick second item from that array
   const token = authHeader.split(' ')[1];
@@ -17,14 +16,14 @@ module.exports = (req, res, next) => {
   try {
     decodedToken = jwt.verify(token, SECRET_KEY);
   } catch (error) {
-    error.statusCode = 500;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
   if (!decodedToken) {
-    const error = new Error('Not authenticated');
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
   req.userId = decodedToken.userId;
+  req.isAuth = true;
   next();
 };
