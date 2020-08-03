2aff084693a81f3910cbfda784b0800d3a5fa250
Yevhwnii <mirandoos3223@gmail.com>
2020-08-03 13:13:13 +0300

    # enabling graphql

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const { graphqlHTTP } = require('express-graphql');
 // Local imports
-
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 // Global variables
 const port = process.env.SERVER_PORT;
 const app = express();
@@ -42,6 +44,16 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
+
 // Global error handler
 app.use((error, req, res, next) => {
   console.log(error);
