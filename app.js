e771a46ab0765cdb4d64108e2da89c6b56afba20
Yevhwnii <mirandoos3223@gmail.com>
2020-08-03 13:41:18 +0300

    # sending response to OPTIONS method

--- app.js
+++ app.js
@@ -42,6 +42,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Since express-graphql rejects all requests other than GET and POST and browser
+  // before such request sends OPTION request, we need to respond to the browser so we can move on
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
